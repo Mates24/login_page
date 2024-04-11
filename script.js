c5eccaf97a929f96d9e7310956c436baae8f2320
Mates24 <matejcikmathias@gmail.com>
2024-04-11 12:09:22 +0200

    Transition between login and signup

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const loginform = document.querySelector('.container .login');
-const signupform = document.querySelector('.container .signup');
+const signupform = document.querySelector('.container div.signup');
 const loginbutton = document.querySelector('p.login-btn span');
 const signupbutton = document.querySelector('p.signup-btn span');
 
-loginbutton.addEventListener('click', () => {
-    signupform.style.right = "200%";
+loginbutton.onclick = () => {
+    signupform.style.right = "200%"
     loginform.style.left = "50%";
-});
+};
 signupbutton.addEventListener('click', () => {
-    signupform.style.right = "50%";
+    signupform.style.right = "50%"
     loginform.style.left = "200%";
 })
